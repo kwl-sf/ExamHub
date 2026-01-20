a3c95d943b823bcf76431058b67515938318bf9c
Khaoula <kwlsf04@gmail.com>
2026-01-20 10:31:41 +0100

    Resolve merge conflicts

--- ExamHub_temp/Project/Public/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $db = new PDO("sqlite:../database/data.sqlite");
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Database error: " . $e->getMessage();
-}
-?>
